f50b877deeed7df3f480d89ec574627df5aef116
Esh527 <edesrihari33@gmail.com>
2023-04-11 02:34:10 +0000

    solution

---
+++ src/components/Counter/index.js
@@ -0,0 +1,40 @@
+import {Component} from 'react'
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+  onIncreament = () => {
+    this.setState(preCount => {
+      console.log(`The previous count is ${preCount.count}`)
+      return {
+        count: preCount.count + 1,
+      }
+    })
+  }
+  onDecreament = () => {
+    this.setState(preCount => {
+      console.log(`The previous count is ${preCount.count}`)
+      return {
+        count: preCount.count - 1,
+      }
+    })
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count">{count}</p>
+        <div>
+          <button className="button" onClick={this.onIncreament}>
+            Increase
+          </button>
+          <button className="button" onClick={this.onDecreament}>
+            Decrease
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Counter
